d56d6529b7d14b95b66ec918bee7ddf9d4565fdf
roylines <roy@roylines.co.uk>
2013-04-26 13:53:15 +0100

    added non-middleware tests

--- test/angular-resource-test.js
+++ test/angular-resource-test.js
@@ -1,8 +1,54 @@
 var assert = require('assert'),
-		angularResource = require('../lib/angular-resource');
+		angularResource = require('../lib/angular-resource'),
+		sinon = require('sinon'),
+		testObject = require('./testObject');
 
-describe('default', function(){
-	it('should not throw', function() {
-		assert(true);
+describe('angular-resource', function() {
+	var app;
+	beforeEach(function() {
+		app = {
+			"get": sinon.stub(),
+			"post": sinon.stub(),
+			"delete": sinon.stub()
+		};
 	});
+	describe('without middleware', function() {
+		describe('using testObject', function() {
+			beforeEach(function() {
+				angularResource(app, '/api', 'testObject');
+			});
+			it('should bind get', function() {
+				assert(app.get.calledWith('/api/testObject/:id', testObject.get));
+			});
+			it('should bind save', function() {
+				assert(app.post.calledWith('/api/testObject/:id', testObject.save));
+			});
+			it('should bind query', function() {
+				assert(app.get.calledWith('/api/testObject', testObject.query));
+			});
+			it('should bind remove', function() {
+				assert(app["delete"].calledWith('/api/testObject/:id', testObject.remove));
+			});
+		});
+	});
+	// describe('with middleware', function() {
+	// 	var middleware = { };
+	// 	describe('using testObject', function() {
+	// 		beforeEach(function() {
+	// 			angularResource(app, '/api', 'testObject');
+	// 		});
+	// 		it('should bind get', function() {
+	// 			assert(app.get.calledWith('/api/testObject/:id', testObject.get));
+	// 		});
+	// 		it('should bind save', function() {
+	// 			assert(app.post.calledWith('/api/testObject/:id', testObject.save));
+	// 		});
+	// 		it('should bind query', function() {
+	// 			assert(app.get.calledWith('/api/testObject', testObject.query));
+	// 		});
+	// 		it('should bind remove', function() {
+	// 			assert(app["delete"].calledWith('/api/testObject/:id', testObject.remove));
+	// 		});
+	// 	});
+	// });
 });
--- test/testObject.js
+++ test/testObject.js
@@ -1,3 +1,15 @@
 var testObject = { };
 
+testObject.get = function() {
+};
+
+testObject.save = function() {
+};
+
+testObject.query = function() {
+};
+
+testObject.remove = function() {
+};
+
 module.exports = testObject;
